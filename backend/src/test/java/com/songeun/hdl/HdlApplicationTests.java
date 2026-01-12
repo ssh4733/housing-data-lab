f90334f090b36620a00cefc72d7d95358023787d
ssh4733 <ssh4733@woongjin.co.kr>
2026-01-12 15:26:55 +0900

    [INIT] 0112 백엔드 환경 구축

---
+++ backend/src/test/java/com/songeun/hdl/HdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songeun.hdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
