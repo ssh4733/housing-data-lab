05b2f32cf6c4969d5c2a5004700785118deaae12
Eungyeong Lee <eunpione22@gmail.com>
2026-02-11 17:45:59 +0900

    [INIT] 0211 project 구조 및 초기 화면 세팅

---
+++ hdl-react-app/src/pages/Homepage.tsx
@@ -0,0 +1,8 @@
+export const HomePage = () => {
+  return (
+    <div>
+      <h1>React 화면 테스트</h1>
+      <p>아파트 매매 실거래가 프로젝트</p>
+    </div>
+  );
+};
--- hdl-react-app/vite.config.ts
+++ hdl-react-app/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias:{
+      "@": fileURLToPath(new URL("./src", import.meta.url)
+      )
+    }
+  }
 })
